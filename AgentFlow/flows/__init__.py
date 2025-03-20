e74c8df2fd99e7572a60bd61773b8284c9ebcae0
jiangbo <jiangbo@zhejianglab.com>
2025-03-20 14:50:52 +0800

    add auto schedule flow

--- AgentFlow/flows/__init__.py
+++ AgentFlow/flows/__init__.py
@@ -1,6 +1,7 @@
 from ..data_model import flowDetailParam, FlowTypeEnum
-from .loop_flow import LoopFlow
+from .loop_flow import SequentialLoopFlow, AutoSchedLoopFlow
 from .sequential_flow import SequentialFlow
+from .auto_sched_flow import AutoSchedFlow
 from .base_flow import BaseFlow
 
 from typing import Union, Dict
@@ -10,10 +11,14 @@ class FlowFactory:
     def create_flow(flow_type: str, config: Union[Dict, flowDetailParam]) -> BaseFlow:
         if flow_type == FlowTypeEnum.SEQUENTIAL:
             return SequentialFlow(config)
+        elif flow_type == FlowTypeEnum.AUTOSCHED:
+            return AutoSchedFlow(config)
         elif flow_type == FlowTypeEnum.CONDITIONAL:
             raise NotImplementedError("Conditional flow is not implemented yet")
-        elif flow_type == FlowTypeEnum.LOOP:
-            return LoopFlow(config)
+        elif flow_type == FlowTypeEnum.SEQUENTIALLOOP:
+            return SequentialLoopFlow(config)
+        elif flow_type == FlowTypeEnum.AUTOSCHEDLOOP:
+            return AutoSchedLoopFlow(config)
         else:
             raise ValueError(f"Unknown flow type: {flow_type}")
 
